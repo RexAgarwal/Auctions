8de966ee0b82008fb9a633157f3bb19b874162ee
RexAgarwal <himanshuagarwal654@gmail.com>
2023-10-26 17:08:50 +0530

    Django app added

---
+++ auctions/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from .models import *
+
+# Register your models here.
+admin.site.register(Auction_listing)
+admin.site.register(Bids)
+admin.site.register(Watchlist)
+admin.site.register(Comments)
+admin.site.register(User)
